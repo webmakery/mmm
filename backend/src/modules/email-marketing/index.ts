507be68ae3124ee909e0525519fbc5ac26308e50
taskpul <romtietz@gmail.com>
2026-04-21 10:36:03 +0200

    Fix email marketing module initialization cycle

--- backend/src/modules/email-marketing/index.ts
+++ backend/src/modules/email-marketing/index.ts
@@ -2,8 +2,9 @@ import { Module } from "@medusajs/framework/utils"
 import seedDefaultEmailTemplates from "./loaders/seed-default-templates"
 import startEmailMarketingCampaignProcessor from "./loaders/start-campaign-processor"
 import EmailMarketingModuleService from "./service"
+import { EMAIL_MARKETING_MODULE } from "./constants"
 
-export const EMAIL_MARKETING_MODULE = "emailMarketing"
+export { EMAIL_MARKETING_MODULE } from "./constants"
 
 export default Module(EMAIL_MARKETING_MODULE, {
   service: EmailMarketingModuleService,
